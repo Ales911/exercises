0e86e1b6611d7f7f20ba74648c0ef5b49f40d082
Ales911 <ales911.nikol@gmail.com>
2024-02-23 18:42:07 +0300

    More exercises added

---
+++ src/main/java/com/example/threads/FootExample.java
@@ -0,0 +1,54 @@
+package com.example.threads;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Foot implements Runnable {
+
+    static final String LEFT = "left";
+    static final String RIGHT = "right";
+
+    private static final AtomicBoolean IS_LEFT_ORDER = new AtomicBoolean(true);
+
+    private final String name;
+
+    public Foot(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            step();
+        }
+    }
+
+    private /*synchronized*/ void step() {
+        
+        if ((name.equals(LEFT) && IS_LEFT_ORDER.get()) || (name.equals(RIGHT) && !IS_LEFT_ORDER.get())) {
+            System.out.println("Step by " + name);
+            if (IS_LEFT_ORDER.get()) {
+                IS_LEFT_ORDER.compareAndSet(true, false);
+            } else {
+                IS_LEFT_ORDER.compareAndSet(false, true);
+            }
+        } else {
+            // System.out.println("Skip by " + name);
+            // Thread.yield();
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Foot.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
+
+public class FootExample {
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(new Foot(Foot.LEFT)).start();
+        new Thread(new Foot(Foot.RIGHT)).start();
+    }
+}
