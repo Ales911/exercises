46306209b207639443c3d31a8712557eff5035ff
Ales911 <ales911.nikol@gmail.com>
2024-03-21 15:59:47 +0300

    txt  updated

--- src/main/java/com/example/exercises/threads/ExampleFutureTask.java
+++ src/main/java/com/example/exercises/threads/ExampleFutureTask.java
@@ -4,12 +4,11 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.FutureTask;
 
 public class ExampleFutureTask {
-    public static void main(String []args) throws Exception {
-        Callable task = () -> {
-            return "Hello, World!";
-        };
+
+    public static void main(String[] args) throws Exception {
+        Callable task = () -> "Hello, World!";
         FutureTask<String> future = new FutureTask<>(task);
         new Thread(future).start();
         System.out.println(future.get());
-    }    
+    }
 }
